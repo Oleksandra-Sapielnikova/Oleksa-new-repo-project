b3f25c1df6373d7c783b75700c6ecc82f8fae5f3
Oleksandra-Sapielnikova <73056445+Oleksandra-Sapielnikova@users.noreply.github.com>
2022-08-07 22:59:45 +0200

    added weather icon

--- src/js.js
+++ src/js.js
@@ -35,6 +35,12 @@ function showActualData(response) {
   );
   document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
